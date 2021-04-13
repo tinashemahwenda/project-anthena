b5b85b3e75d078264790f29dd5aae200e2ef4d8a
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2021-04-13 13:25:17 +0200

    deleted files

--- my_site/demo/urls.py
+++
@@ -1,7 +0,0 @@
-from .views import HomePageView,AboutPageView
-from django.urls import path,include
-
-urlpatterns = [
-    path('',HomePageView.as_view(),name='home'),
-    path('about/',AboutPageView.as_view(),name='about')
-]
--- my_site/demo/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
-
-class HomePageView(TemplateView):
-    template_name = 'index.html'
-class AboutPageView(TemplateView):
-    template_name = 'about.html'
