418e71994809badb0fbe8fef175a7913113fcc16
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2021-04-13 07:45:41 +0200

    New

--- my_pages/pages/config/pages/urls.py
+++
@@ -1,7 +0,0 @@
-from .views import HomePageView,AboutPageView
-from django.urls import path
-
-urlpatterns = [
-   path('',HomePageView.as_view(),name='home'),
-   path('about/',AboutPageView.as_view(),name='about')
-]
